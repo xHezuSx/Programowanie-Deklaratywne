dba3c989f171412794441dbe4a356b22199d7674
xHezuSx <jakubek.zakowski@gmail.com>
2024-05-08 13:31:18 +0200

    aktualizacja lekcji

---
+++ lab1/klocki1.pl
@@ -0,0 +1,18 @@
+% PROGRAM: klocki_1
+% Baza wiedzy o uk³adzie klocków
+% Definiowanie predykaty:
+% na/2
+%==============================================
+
+
+% na(X,Y)
+% opis: spe³niony, gdy klocek x le¿y
+% bezpoœrednio na klocku Y
+% -------------------------------------------------
+
+na(c,a).
+na(c,b).
+na(d,c).
+
+% inforamcje o budowie programu:
+% program sk³ada siê  3 klauzul.
